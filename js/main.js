db0d35e9fa8bec5837e4b83d30f1b0174ca31e69
Sergey Str <58007220+StrSergey@users.noreply.github.com>
2020-07-14 04:09:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const progress = document.querySelector('.progress');
+
+window.addEventListener('scroll', progressBar);
+
+
+function progressBar(e) {
+  let windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  let windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  let per = (windowScroll / windowHeight) * 100;
+
+  progress.style.width = per + "%";
+}
+
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+//   document.getElementById("myBar").style.width = scrolled + "%";
+// }
